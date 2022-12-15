b989aa28818d088b46b3a17149bb6e7b4d1e264c
sagarbisht07 <bishtsagarbisht1232gmail.com>
2022-12-15 22:38:56 +0530

    day2 done

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ cartIcon.onclick=()=>{
 closecart.onclick=()=>{
     cart.classList.remove("active");
 };
+
+// slider js starts here
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+// slider js end here 
