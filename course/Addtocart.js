588e4360b5e392da66019db17000384bdb464388
subrat199 <subratt01998@gmail.com>
2022-12-16 20:46:11 +0530

    day4

--- course/Addtocart.js
+++ course/Addtocart.js
@@ -1,17 +1,30 @@
-let all_addtocart_button=document.querySelectorAll(".add")
+let addtocart_button=document.querySelectorAll(".add")
+let getitem=JSON.parse(localStorage.getItem("add_to_cart_item"))
 let arr=[]
 let obj={}
-for(let el of all_addtocart_button){
-    el.addEventListener("click",(e)=>{
-        e.preventDefault()
-        console.log(e)
-        el.textContent="CHECKOUT"
-        obj["img"]=e.path[2].children[0].currentSrc
-        obj["product_name"]=e.path[2].children[1].innerHTML
-        obj["price"]=e.path[2].children[4].innerText
-        console.log(obj)
-        arr.push(obj)
-        console.log(arr)
+for(let el of addtocart_button){
+    el.addEventListener("click",(e)=>{   
+    el.textContent="CHECKOUT"
+         obj["img"]="https://assets.interntheory.com/creative/courses/thumbnails/it.jpg"
+         obj["product_name"]="IT Starter Pack (4 Courses)"
+         obj["price"]=15999;
+   
+         arr.push(obj)
         localStorage.setItem("add_to_cart_item",JSON.stringify(arr))
     })
 }
+    getitem.forEach((el)=>{
+        let div=document.createElement("div")
+        
+        let img=document.createElement("img")
+        img.setAttribute("src",el.img)
+        let prname=document.createElement("h4")
+        prname.innerText=el.price
+        let prprice=document.createElement("p")
+        prprice.innerText=el.product_name
+        div.append(img,prname,prprice)
+        document.querySelector(".display_addtocart_item").append(div)
+    })
+
+
+console.log(getitem)
